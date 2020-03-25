2249075c9edaeacb7ca078323499c8bb7548355b
CBreakr <github email address>
2020-03-25 11:06:10 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(attributes)
+    name_att = attributes["username"]
+    if name_att.present? then
+      self.user = User.find_or_create_by(username: name_att)
+    end
+  end
 end
